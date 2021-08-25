039318493cb88a49290520f92190f29bd9fccbe6
Maicon Filippsen <filippsen@blockie.org>
2021-08-25 10:03:28 -0300

    Move client/server tests to new testy file

---
+++ test/connection.spec.ts
@@ -0,0 +1,55 @@
+import { TestSuite, Test } from "testyts";
+import {TCPClient, WSClient} from "../index";
+import {TCPServer, WSServer, AbstractClient} from "../index";
+
+const assert = require("assert");
+
+@TestSuite()
+export class Connection {
+
+    @Test()
+    public async clientserver() {
+        await new Promise(resolve => {
+            const serverOptions = {
+                host: "localhost",
+                port: 8181
+            };
+            const server = new WSServer(serverOptions);
+            server.listen();
+
+            server.onConnection( (client: AbstractClient) => {
+                console.log("Socket accepted");
+                client.onData( (data: Buffer) => {
+                    console.log("Incoming data", data);
+                    assert(data.toString() == "hello");
+                    client.sendString("received!");
+                });
+                client.onDisconnect( () => {
+                    console.log("client disconnected");
+                    server.close();
+                    resolve();
+                });
+            });
+
+            const clientOptions = {
+                host: "localhost",
+                port: 8181
+            };
+            const client = new WSClient(clientOptions);
+            client.connect();
+
+            client.onConnect( () => {
+                console.log("Client connected");
+                client.onData( (data: Buffer) => {
+                    console.log("Incoming data", data);
+                    assert(data.toString() == "received!");
+                    client.disconnect();
+                });
+                client.onDisconnect( () => {
+                    console.log("client disconnected");
+                });
+                client.sendString("hello");
+            });
+        });
+    }
+}
