0b24a8f91c9081ab30f35f86b253c770088eab0d
Maicon Filippsen <filippsen@blockie.org>
2021-09-03 09:56:19 -0300

    Increment WSClient tests
    + Add test against initialization with existing socket
    
    + Add more checks around socketHook tests
    
    + Add new noop socketHook test call when socket is missing
    
    + Add new set of socketConnect tests
    
    + Add new noop socketSend test call when socket is missing
    
    + Add new noop socketClose test call when socket is missing
    
    + Add client error verification

--- test/WSClient.spec.ts
+++ test/WSClient.spec.ts
@@ -4,6 +4,7 @@ import {WSClient} from "../index";
 const assert = require("assert");
 const net = require("net");
 const tls = require("tls");
+const ws = require("ws");
 
 @TestSuite()
 export class WSClientSocketConstructor {
@@ -40,8 +41,29 @@ export class WSClientSocketConstructor {
             });
         });
     }
-}
 
+    @Test()
+    public successful_call_with_existing_socket() {
+        assert.doesNotThrow(() => {
+            let flag = false;
+            class TestClient extends WSClient {
+                socketHook() {
+                    flag = true;
+                }
+            }
+            assert(flag == false);
+            //@ts-ignore incomplete socket definition
+            let socket: ws.WebSocket = {};
+            //@ts-ignore incomplete socket definition
+            const client = new TestClient({
+                "host": "host.com",
+                "port": 99,
+                "secure": false,
+            }, /*@ts-ignore*/ socket);
+            assert(flag == true);
+        });
+    }
+}
 
 @TestSuite()
 export class WSClientSocketHook {
@@ -62,10 +84,60 @@ export class WSClientSocketHook {
             //@ts-ignore: validate flow
             client.socketConnect();
             assert(!client.socket!.onmessage);
+            assert(!client.socket!.onerror);
+            assert(!client.socket!.onclose);
             //@ts-ignore: protected method
             client.socketHook();
             assert(client.socket!.onmessage);
+            assert(client.socket!.onerror);
+            assert(client.socket!.onclose);
         });
+    }
+
+    @Test()
+    public missing_socket_noop() {
+        assert.doesNotThrow(() => {
+            const client = new WSClient({
+                "host": "host.com",
+                "port": 99,
+                "secure": false,
+            });
+            client.socket = undefined;
+            //@ts-ignore: protected method
+            client.socketHook();
+        });
+    }
+}
+
+@TestSuite()
+export class WSClientSocketConnect {
+    @Test()
+    public socket_already_created() {
+        assert.throws(() => {
+            //@ts-ignore incomplete socket definition
+            let socket: ws.WebSocket = {};
+            //@ts-ignore incomplete socket definition
+            const client = new WSClient({
+                "host": "host.com",
+                "port": 99,
+                "secure": false,
+            }, /*@ts-ignore*/ socket);
+            assert(client.socketConnect());
+        }, /Socket already created./);
+    }
+
+    @Test()
+    public undefined_options() {
+        assert.throws(() => {
+            const client = new WSClient({
+                "host": "host.com",
+                "port": 99,
+                "secure": false,
+            });
+            client.clientOptions = undefined;
+            //@ts-ignore: protected method
+            assert(client.socketConnect());
+        }, /clientOptions is required to create socket./);
     }
 }
 
@@ -92,10 +164,25 @@ export class WSClientSocketSend {
             client.socketSend(Buffer.from("testdata123"));
         });
     }
+
+    @Test()
+    public missing_socket_noop() {
+        assert.doesNotThrow(() => {
+            const client = new WSClient({
+                "host": "host.com",
+                "port": 99,
+                "secure": false,
+            });
+            client.socket = undefined;
+            //@ts-ignore: protected method
+            client.socketSend(Buffer.from("testdata123"));
+        });
+    }
+
 }
 
 @TestSuite()
-export class WSClientSocketDisconnect {
+export class WSClientSocketClose {
     @Test()
     public successful_call() {
         assert.doesNotThrow(() => {
@@ -117,6 +204,45 @@ export class WSClientSocketDisconnect {
             client.socketClose();
             //@ts-ignore: data expected to be changed by previously called inner functions
             assert(hasClosed == true);
+        });
+    }
+
+    @Test()
+    public missing_socket_noop() {
+        assert.doesNotThrow(() => {
+            const client = new WSClient({
+                "host": "host.com",
+                "port": 99,
+                "secure": false,
+            });
+            client.socket = undefined;
+            //@ts-ignore: protected method
+            client.socketClose();
+        });
+    }
+}
+
+@TestSuite()
+export class WSClientError {
+    @Test()
+    public successful_call() {
+        assert.doesNotThrow(() => {
+            const client = new WSClient({
+                 "host": "host.com",
+                "port": 99,
+                "secure": false,
+            });
+            let flag = false;
+            //@ts-ignore: protected method
+            client.socketError = function(buffer: Buffer) {
+                assert(buffer.toString() == "test error");
+                flag = true;
+            };
+            assert(flag == false);
+            //@ts-ignore: protected method
+            client.error(new Error("test error"));
+            //@ts-ignore: flag changes inside custom callback
+            assert(flag == true);
         });
     }
 }
