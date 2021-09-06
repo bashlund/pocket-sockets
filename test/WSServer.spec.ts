6afff798609cd0a3738a28bced5c4edea45e0604
Maicon Filippsen <filippsen@blockie.org>
2021-09-06 14:47:56 -0300

    Include additional WSServer tests
    + Add new WSServer constructor test
    
    + Add new WSServer test verifying createServer call
    
    + Add more checks to existing serverListen test
    
    + Add new clientConnected test
    
    + Add new serverError verification

--- test/WSServer.spec.ts
+++ test/WSServer.spec.ts
@@ -9,6 +9,35 @@ const WebSocket = require("ws");
 @TestSuite()
 export class WSServerConstructor {
     @Test()
+    public calls_serverCreate() {
+        assert.doesNotThrow(() => {
+            https.createServer = function() {
+                return {
+                    "on": function(name: string, fn: Function) {
+                        assert(name == "secureConnection" || name == "error" || name == "close" || name == "tlsClientError");
+                        assert(typeof fn == "function");
+                    }
+                };
+            };
+            let flag = false;
+            class TestServer extends WSServer {
+                serverCreate() {
+                    flag = true;
+                }
+            }
+            assert(flag == false);
+            const server = new TestServer({
+                "host": "host.com",
+                "port": 99,
+                "cert": "valid-certificate"
+            });
+            //@ts-ignore: modified by custom serverCreate
+            assert(flag == true);
+        });
+    }
+
+
+    @Test()
     public successful_call_with_cert() {
         assert.doesNotThrow(() => {
             https.createServer = function() {
@@ -86,6 +115,20 @@ export class WSServerCreate {
             assert(server.server);
         });
     }
+
+    @Test()
+    public undefined_return_from_createServer() {
+        assert.doesNotThrow(() => {
+            http.createServer = function() {
+                return undefined;
+            };
+            const server = new WSServer({
+                "host": "host.com",
+                "port": 99,
+            });
+            assert(!server.server);
+        });
+    }
 }
 
 @TestSuite()
@@ -129,7 +172,11 @@ export class WSServerListen {
                 };
             };
 
-            WebSocket.Server = function() {
+            WebSocket.Server = function(data: any) {
+                assert(data!.path == "/");
+                assert(data!.clientTracking == true);
+                assert(data!.perMessageDeflate == false);
+                assert(data!.maxPayload == 100*1024*1024);
                 return {
                     "on": function(name: string, fn: Function) {
                         assert(name == "connection" || name == "error" || name == "close"); 
@@ -143,8 +190,10 @@ export class WSServerListen {
                 "port": 99,
                 "cert": "valid-certificate"
             });
+            assert(!server.wsServer);
             //@ts-ignore: protected method
             server.serverListen();
+            assert(server.wsServer);
         });
     }
 }
@@ -189,6 +238,62 @@ export class WSServerClose {
             //@ts-ignore: protected method
             server.serverClose();
             assert(callCount == 2);
+        });
+    }
+}
+
+@TestSuite()
+export class WSServerClientConnected {
+    @Test()
+    public pass_socket_to_clientConnected() {
+        assert.doesNotThrow(() => {
+            https.createServer = function() {
+                return {
+                    "on": function(name: string, fn: Function) {
+                        assert(name == "connection" || name == "error" || name == "close" || name == "tlsClientError");
+                        assert(typeof fn == "function");
+                    }
+                };
+            };
+            const server = new WSServer({
+                "host": "host.com",
+                "port": 99,
+            });
+            assert(server.clients.length == 0);
+            //@ts-ignore: protected method
+            server.clientConnected(server);
+            assert(server.clients.length == 1);
+        });
+    }
+}
+@TestSuite()
+export class WSServerError {
+    @Test()
+    public successful_call() {
+        assert.doesNotThrow(() => {
+            https.createServer = function() {
+                return {
+                    "on": function(name: string, fn: Function) {
+                        assert(name == "connection" || name == "error" || name == "close" || name == "tlsClientError");
+                        assert(typeof fn == "function");
+                    }
+                };
+            };
+            const server = new WSServer({
+                "host": "host.com",
+                "port": 99,
+            });
+            let flag = false;
+            //@ts-ignore: protected method
+            server.serverError = function(buffer: Buffer) {
+                assert(buffer.toString() == "test error");
+                flag = true;
+            };
+            assert(flag == false);
+            //@ts-ignore: protected method
+            server.error(new Error("test error"));
+            //@ts-ignore: flag changes inside custom callback
+            assert(flag == true);
         });
     }
 }
