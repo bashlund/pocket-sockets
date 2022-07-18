40b164cc2a8d32fdb06b0640d163e78792ae8e5b
Maicon Filippsen <filippsen@blockie.org>
2022-07-18 11:18:29 -0300

    Ensure WSClient IPv6 host gets surrounded with brackets during socketConnect
    + Add 3 new connection tests verifying different cases of IPv4 and IPv6 host strings

--- test/connection.spec.ts
+++ test/connection.spec.ts
@@ -48,6 +48,126 @@ export class Connection {
     }
 
     @Test()
+    public async clientserver_ipv4() {
+        await new Promise(resolve => {
+            const serverOptions = {
+                host: "127.0.0.1",
+                port: 8181
+            };
+            const server = new WSServer(serverOptions);
+            server.listen();
+
+            server.onConnection( (client: Client) => {
+                client.onData( (data: Buffer) => {
+                    assert(data.toString() == "hello");
+                    client.sendString("received!");
+                });
+                client.onClose( () => {
+                    server.close();
+                    resolve();
+                });
+            });
+
+            const clientOptions = {
+                host: "127.0.0.1",
+                port: 8181
+            };
+            const client = new WSClient(clientOptions);
+            client.connect();
+
+            client.onConnect( () => {
+                client.onData( (data: Buffer) => {
+                    assert(data.toString() == "received!");
+                    client.close();
+                });
+                client.onClose( () => {
+                });
+                client.sendString("hello");
+            });
+        });
+    }
+
+    @Test()
+    public async clientserver_ipv6_short() {
+        await new Promise(resolve => {
+            const serverOptions = {
+                host: "::1",
+                port: 8181
+            };
+            const server = new WSServer(serverOptions);
+            server.listen();
+
+            server.onConnection( (client: Client) => {
+                client.onData( (data: Buffer) => {
+                    assert(data.toString() == "hello");
+                    client.sendString("received!");
+                });
+                client.onClose( () => {
+                    server.close();
+                    resolve();
+                });
+            });
+
+            const clientOptions = {
+                host: "::1",
+                port: 8181
+            };
+            const client = new WSClient(clientOptions);
+            client.connect();
+
+            client.onConnect( () => {
+                client.onData( (data: Buffer) => {
+                    assert(data.toString() == "received!");
+                    client.close();
+                });
+                client.onClose( () => {
+                });
+                client.sendString("hello");
+            });
+        });
+    }
+
+    @Test()
+    public async clientserver_ipv6_long() {
+        await new Promise(resolve => {
+            const serverOptions = {
+                host: "0:0:0:0:0:0:0:1",
+                port: 8181
+            };
+            const server = new WSServer(serverOptions);
+            server.listen();
+
+            server.onConnection( (client: Client) => {
+                client.onData( (data: Buffer) => {
+                    assert(data.toString() == "hello");
+                    client.sendString("received!");
+                });
+                client.onClose( () => {
+                    server.close();
+                    resolve();
+                });
+            });
+
+            const clientOptions = {
+                host: "0:0:0:0:0:0:0:1",
+                port: 8181
+            };
+            const client = new WSClient(clientOptions);
+            client.connect();
+
+            client.onConnect( () => {
+                client.onData( (data: Buffer) => {
+                    assert(data.toString() == "received!");
+                    client.close();
+                });
+                client.onClose( () => {
+                });
+                client.sendString("hello");
+            });
+        });
+    }
+
+    @Test()
     public async clientserver_missing_host() {
         await new Promise(resolve => {
             const serverOptions = {
