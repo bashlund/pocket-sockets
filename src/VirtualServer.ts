adce50a31f23e0b09a8c815635ff3f8e4ecf1ec7
Thomas Backlund <ts.backlund@gmail.com>
2022-08-25 20:20:59 +0200

    Add VirtualServer class to complement VirtualClient
    This will be useful for testing as we can go full virtual also on server sockets

---
+++ src/VirtualServer.ts
@@ -0,0 +1,58 @@
+import {
+    Server,
+} from "./Server";
+
+import {
+    VirtualClient,
+} from "./VirtualClient";
+
+import {
+    ServerOptions,
+} from "./types";
+
+/**
+ * Simulate a server socket listening.
+ */
+export class VirtualServer extends Server
+{
+    /**
+     * @param serverOptions are ignored.
+     */
+    constructor(serverOptions: ServerOptions) {
+        super(serverOptions);
+        this.serverCreate();
+    }
+
+    /**
+     * Simulate that a client has connected.
+     * @param client the client which has connected to this server.
+     * Note that the given client must also be paired with its counterpart.
+     */
+    public simulateConnection(client: VirtualClient) {
+        this.addClient(client);
+    }
+
+    /**
+     * Trigger an error event.
+     */
+    public simulateError(error: Error) {
+        this.serverError(Buffer.from(error.message));
+    }
+
+    /**
+     * Specifies how the server gets initialized, then creates the server with the specified options.
+     */
+    protected serverCreate()
+    {
+        // Do nothing
+    }
+
+    protected serverListen()
+    {
+        // Do nothing
+    }
+
+    protected serverClose() {
+        this.serverClosed();
+    }
+}
