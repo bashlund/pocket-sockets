578d8a9412b9b6b757ffc5e63c58a5f59eab0335
Maicon Filippsen <filippsen@blockie.org>
2022-09-15 12:06:33 -0300

    Add more SocketFactory tests

--- test/SocketFactory.spec.ts
+++ test/SocketFactory.spec.ts
@@ -60,6 +60,7 @@ export class SocketFactoryConstructor {
     }
 }
 
+@TestSuite()
 export class SocketFactoryInit {
     @Test()
     public successful_call_with_client_and_server_configurations() {
@@ -170,4 +171,313 @@ export class SocketFactoryInit {
         });
     }
 
+    @Test()
+    public successful_call_without_configurations() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({});
+            let openServerCalled = false;
+            //@ts-ignore
+            socketFactory.openServer = function() {
+                openServerCalled = true;
+            };
+            let connectClientCalled = false;
+            //@ts-ignore
+            socketFactory.connectClient = function() {
+                connectClientCalled = true;
+            };
+            assert(openServerCalled == false);
+            assert(connectClientCalled == false);
+            socketFactory.init();
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(openServerCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(connectClientCalled == false);
+        });
+    }
+}
+
+@TestSuite()
+export class SocketFactoryGetSocketFactoryConfig {
+    @Test()
+    public successful_call() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                maxConnections: 3
+            });
+            const socketFactoryConfig = socketFactory.getSocketFactoryConfig();
+            assert(Object.keys(socketFactoryConfig).length == 1);
+            assert(socketFactoryConfig.maxConnections == 3);
+        });
+    }
+}
+
+@TestSuite()
+export class SocketFactoryConnectClient {
+    @Test()
+    public successful_call() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                client: {
+                    socketType: "TCP",
+                    clientOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    reconnectDelay: 0,
+                },
+                server: {
+                    socketType: "TCP",
+                    serverOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    deniedIPs: ["192.168.5.5"],
+                    allowedIPs: ["127.0.0.1", "localhost"],
+                },
+            });
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                createClientSocketCalled = true;
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory.connectClient();
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == true);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == true);
+        });
+    }
+
+    @Test()
+    public already_connected() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                client: {
+                    socketType: "TCP",
+                    clientOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    reconnectDelay: 0,
+                },
+                server: {
+                    socketType: "TCP",
+                    serverOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    deniedIPs: ["192.168.5.5"],
+                    allowedIPs: ["127.0.0.1", "localhost"],
+                },
+            });
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                // return dummy client as boolean instead
+                return true;
+            };
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+            };
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                createClientSocketCalled = true;
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == false);
+        });
+    }
+
+    @Test()
+    public isClosed() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                client: {
+                    socketType: "TCP",
+                    clientOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    reconnectDelay: 0,
+                },
+            });
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                createClientSocketCalled = true;
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory._isClosed = true;
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == false);
+        });
+    }
+
+    @Test()
+    public isShutdown() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                client: {
+                    socketType: "TCP",
+                    clientOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    reconnectDelay: 0,
+                },
+            });
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                createClientSocketCalled = true;
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory._isShutdown = true;
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == false);
+        });
+    }
+
+    @Test()
+    public missing_client_configuration() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({});
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                createClientSocketCalled = true;
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == false);
+        });
+    }
+
+    @Test()
+    public checkConnectionsOverflow_true() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                client: {
+                    socketType: "TCP",
+                    clientOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    reconnectDelay: 0,
+                },
+            });
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                createClientSocketCalled = true;
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory.checkConnectionsOverflow = function() {
+                return true;
+            };
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == false);
+        });
+    }
+
+    @Test()
+    public createClientSocket_throws() {
+        assert.doesNotThrow(() => {
+            let socketFactory = new SocketFactory({
+                client: {
+                    socketType: "TCP",
+                    clientOptions: {
+                        "host": "host.com",
+                        "port": 99
+                    },
+                    reconnectDelay: 0,
+                },
+            });
+            let createClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.createClientSocket = function() {
+                throw new Error("test");
+            };
+            let initClientSocketCalled = false;
+            //@ts-ignore
+            socketFactory.initClientSocket = function() {
+                initClientSocketCalled = true;
+            };
+            //@ts-ignore
+            socketFactory.triggerEvent = function(name, args) {
+                assert(name == "CLIENT_INIT_ERROR" || name == "ERROR");
+                if(args.e && args.e.error) {
+                    assert(args.e.error.message == "test");
+                } else {
+                    assert(args.error.message == "test");
+                }
+            };
+
+            //@ts-ignore
+            socketFactory.connectClient();
+
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(createClientSocketCalled == false);
+            //@ts-ignore: static analysis unable to catch state mutation
+            assert(initClientSocketCalled == false);
+        });
+    }
 }
